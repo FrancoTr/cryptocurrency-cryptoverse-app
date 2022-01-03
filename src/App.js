529d7c6efe06b15e5c1fb1c125ef544611bbeb97
Franco Traverso <franco-t20@outlook.com>
2022-01-02 21:40:03 -0300

    fix react-router components (Router is no longer used)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter, Link } from "react-router-dom";
 import { Layout, Typography, Space } from "antd";
 
 import { Navbar, Exchanges, Homepage, Cryptocurrencies, News, CryptoDetails } from "./components";
@@ -14,23 +14,21 @@ const App = () => {
       <div className="main">
         <Layout>
           <div className="routes">
-            <Switch>
-              <Route exact path="/">
-                <Homepage />
-              </Route>
-              <Route exact path="/exchanges">
-                <Exchanges />
-              </Route>
-              <Route exact path="/cryptocurrencies">
-                <Cryptocurrencies />
-              </Route>
-              <Route exact path="/crypto/:coinId">
-                <CryptoDetails />
-              </Route>
-              <Route exact path="/news">
-                <News />
-              </Route>
-            </Switch>
+            <Link to="/">
+              <Homepage />
+            </Link>
+            <Link to="/exchanges">
+              <Exchanges />
+            </Link>
+            <Link to="/cryptocurrencies">
+              <Cryptocurrencies />
+            </Link>
+            <Link to="/crypto/:coinId">
+              <CryptoDetails />
+            </Link>
+            <Link to="/news">
+              <News />
+            </Link>
           </div>
         </Layout>
       </div>
