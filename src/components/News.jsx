d8f5e0118f6431eb0ed03c3b2da9715197f341ac
Franco Traverso <franco-t20@outlook.com>
2022-02-02 23:27:37 -0300

    Add news cards to Crypto News section

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -4,8 +4,33 @@ import moment from "moment";
 
 import { useGetCryptoNewsQuery } from "../services/cryptoNewsApi";
 
-const News = () => {
-  return <div>News</div>;
+const { Text, Title } = Typography;
+const { Option } = Select;
+
+const News = ({ simplified }) => {
+  const { data: cryptoNews } = useGetCryptoNewsQuery({
+    newsCategory: "Cryptocurrency",
+    count: simplified ? 10 : 100,
+  });
+
+  if (!cryptoNews?.value) return "Loading...";
+  return (
+    <Row gutter={[24, 24]}>
+      {cryptoNews.value.map((news, i) => (
+        <Col xs={24} sm={12} lg={8} key={i}>
+          <Card hoverable className="news-card">
+            <a href={news.url} target="_blank" ref="noreffer">
+              <div className="news-image-container">
+                <Title className="news-title" level={4}>
+                  {news.name}
+                </Title>
+              </div>
+            </a>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+  );
 };
 
 export default News;
