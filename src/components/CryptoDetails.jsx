b273b40b31f2e3f0ba3bad2f75886643600fa9c9
Franco Traverso <franco-t20@outlook.com>
2022-02-12 23:41:50 -0300

    Add LineChart component

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -16,6 +16,7 @@ import {
 } from "@ant-design/icons";
 
 import { useGetCryptoDetailsQuery } from "../services/cryptoApi";
+import LineChart from "./LineChart";
 
 const { Title, Text } = Typography;
 const { Option } = Select;
@@ -99,6 +100,11 @@ const CryptoDetails = () => {
           <Option key={date}>{date}</Option>
         ))}
       </Select>
+      <LineChart
+        coinHistory={coinHistory}
+        currentPrice={millify(cryptoDetails.price)}
+        coinName={cryptoDetails.name}
+      />
       <Col className="stats-container">
         <Col className="coin-value-statistics">
           <Col className="coin-value-statistics-heading">
