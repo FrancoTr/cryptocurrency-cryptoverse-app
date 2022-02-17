0951a4252a61e7a862a349ab4362affcf6735049
Franco Traverso <franco-t20@outlook.com>
2022-02-16 22:42:16 -0300

    Add data options objects

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -6,12 +6,37 @@ const { Title } = Typography;
 
 const LineChart = ({ coinHistory, currentPrice, coinName }) => {
   const coinPrice = [];
-  const coinTimeStamp = [];
+  const coinTimestamp = [];
 
   for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
     coinPrice.push(coinHistory.data.history[i].price);
-    coinTimeStamp.push(new Date(coinHistory.data.history[i].timestamp).toLocaleDateString());
+    coinTimestamp.push(new Date(coinHistory.data.history[i].timestamp).toLocaleDateString());
   }
+
+  const data = {
+    label: coinTimestamp,
+    datasets: [
+      {
+        label: "Price in USD",
+        data: coinPrice,
+        fill: false,
+        backgroundColor: "#0071bd",
+        borderColor: "#0071bd",
+      },
+    ],
+  };
+
+  const options = {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true,
+          },
+        },
+      ],
+    },
+  };
 
   return (
     <>
